7ec141ae7197d27264170531bc74b6707c4d4711
dinesh <ignoredinesh@gmail.com>
2025-04-17 10:26:09 +0530

    sotage

--- src/book-library/index.jsx
+++ src/book-library/index.jsx
@@ -1,30 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddBook from "./add-book";
 import ListBook from "./list-book";
 
 export default function BookLibrary() {
   const [book, setBook] = useState("new");
   const [list, setList] = useState(["first book", "secpomd book"]);
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    const temp = localStorage.getItem("list")?.split(",") ?? [];
+    console.log(temp);
+    setList(temp);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("list", list.join(","));
+  }, [list]);
   const handleInput = (arg) => {
     setBook(arg);
   };
+
   const handleAdd = () => {
     setList((prev) => [...prev, book]);
     setBook("");
+  };
+  const handleDelete = (arg) => {
+    const temp = list.filter((item) => item !== arg);
+    setList(temp);
+    setHistory((prev) => [...prev, arg]);
   };
   return (
     <div className="container">
       <div className="fs-2">Book library</div>
       <div className="row">
+        <AddBook book={book} handleInput={handleInput} handleAdd={handleAdd} />
         <div className="col-6">
-          <AddBook
-            book={book}
-            handleInput={handleInput}
-            handleAdd={handleAdd}
+          <ListBook
+            title="Book list"
+            showBtn={true}
+            list={list}
+            handleDelete={handleDelete}
           />
-          <ListBook list={list} />
         </div>
-        <div className="col-6"></div>
+        <div className="col-6">
+          <ListBook
+            title="History of Book list deletion"
+            list={history}
+            handleDelete={() => {}}
+          />
+        </div>
       </div>
     </div>
   );
--- src/book-library/list-book.jsx
+++ src/book-library/list-book.jsx
@@ -2,12 +2,26 @@ import React from "react";
 
 export default function ListBook(props) {
   return (
-    <ul className="list-group mt-4">
-      {props.list.map((item) => (
-        <li key={item} className="list-group-item">
-          {item}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h4 className="fs-4 text-primary mt-4">{props.title}</h4>
+      <ul className="list-group ">
+        {props.list.map((item) => (
+          <li
+            key={item}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {item}
+            {props.showBtn && (
+              <button
+                className="btn btn-sm btn-danger"
+                onClick={() => props.handleDelete(item)}
+              >
+                Delete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
